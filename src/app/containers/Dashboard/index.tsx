db95f43fa190cb67c5cdf7547f8302f36eff6491
Alexsandar the Great <alexgreat609520@gmail.com>
2020-11-12 00:40:06 +0500

    Add View Add New Board Section

--- src/app/containers/Dashboard/index.tsx
+++ src/app/containers/Dashboard/index.tsx
@@ -242,12 +242,8 @@ export const Dashboard = memo((props: Props) => {
                       <Dropdown
                         overlay={
                           <Menu>
-                            <Menu.Item key="0">
-                              <a href="http://www.alipay.com/">1st menu item</a>
-                            </Menu.Item>
-                            <Menu.Item key="1">
-                              <a href="http://www.taobao.com/">2nd menu item</a>
-                            </Menu.Item>
+                            <Menu.Item key="0">3rd menu item</Menu.Item>
+                            <Menu.Item key="1">3rd menu item</Menu.Item>
                             <Menu.Divider />
                             <Menu.Item key="3">3rd menu item</Menu.Item>
                           </Menu>
