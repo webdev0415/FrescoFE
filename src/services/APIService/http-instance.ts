ecf1dafcbf604bed6188abac638316e631c4baa4
Alexsandar the Great <alexgreat609520@gmail.com>
2020-12-01 10:01:15 +0500

    On Error Display Error Page Like 400 500 errors

--- src/services/APIService/http-instance.ts
+++ src/services/APIService/http-instance.ts
@@ -1,5 +1,6 @@
-import Axios, { AxiosRequestConfig } from 'axios';
+import Axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import Auth from '../Auth';
+import History from '../History';
 
 export const http = Axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
@@ -12,3 +13,26 @@ http.interceptors.request.use((config: AxiosRequestConfig) => {
 
   return config;
 });
+
+http.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response;
+  },
+  error => {
+    if (!error.response) {
+      error.response = {
+        data: 'Network error',
+        status: 500,
+      };
+      History.push('/500');
+    } else if (error.response.status === 401) {
+      Auth.clearToken();
+      History.push('/login/admin');
+      throw error;
+    } else {
+      History.push('/404');
+      throw error;
+    }
+    return Promise.reject(error);
+  },
+);
