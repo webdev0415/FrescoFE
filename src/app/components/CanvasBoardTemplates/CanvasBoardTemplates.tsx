dd034a546e582663e9001223ea971eab29cf0fd3
igorshevk <igor.shevk@gmail.com>
2020-12-04 08:05:08 -0800

    plus icon

--- src/app/components/CanvasBoardTemplates/CanvasBoardTemplates.tsx
+++ src/app/components/CanvasBoardTemplates/CanvasBoardTemplates.tsx
@@ -147,7 +147,6 @@ export const CanvasBoardTemplates = memo((props: Props) => {
                           <Button
                             block
                             type="primary"
-                            icon={<PlusOutlined />}
                             loading={loadingCreateBoard === board.id}
                             onClick={() => handleCreateBoard(board.id)}
                           >
