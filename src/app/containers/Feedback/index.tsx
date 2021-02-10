6649901fbabf157a67f864542f9e06ea80fc0bf7
Rooshan Ahmed <rooshan.ahmed@hotmail.com>
2021-02-10 12:38:15 +0500

    feeback page updated

--- src/app/containers/Feedback/index.tsx
+++ src/app/containers/Feedback/index.tsx
@@ -29,138 +29,152 @@ const FeedBack = () => {
               style={{ paddingTop: '300px', textAlign: 'center' }}
               bordered={true}
             >
-              <p>
-                Considering your experience using Fresco, how likely would you
-                recommend our tool to a friend or colleague?
-              </p>
-              <div>
-                <Radio.Group buttonStyle="solid" size="large">
-                  <Space size="middle">
-                    <Radio.Button
-                      style={{
-                        border: '1px solid blue',
-                        padding: '0 25px 25px',
-                        color: 'blue',
-                      }}
-                      value={0}
-                    >
-                      0
-                    </Radio.Button>
+              <div style={{ paddingBottom: '64px' }}>
+                <p style={{ fontSize: '16px' }}>
+                  Considering your <i>experience</i> using Fresco, how likely
+                  would you recommend our tool to a friend or colleague?
+                </p>
+                <div>
+                  <Radio.Group buttonStyle="solid" size="large">
+                    <Space size="small">
+                      <Radio.Button
+                        style={{
+                          border: '1px solid #6b91f6',
+                          padding: '0 20px 40px',
+                          color: '#6b91f6',
+                        }}
+                        value={0}
+                      >
+                        0
+                      </Radio.Button>
 
-                    <Radio.Button
-                      style={{
-                        border: '1px solid blue',
-                        padding: '0 25px 25px',
-                        color: 'blue',
-                      }}
-                      value={1}
-                    >
-                      1
-                    </Radio.Button>
-                    <Radio.Button
-                      style={{
-                        border: '1px solid blue',
-                        padding: '0 25px 25px',
-                        color: 'blue',
-                      }}
-                      value={2}
-                    >
-                      2
-                    </Radio.Button>
-                    <Radio.Button
-                      style={{
-                        border: '1px solid blue',
-                        padding: '0 25px 25px',
-                        color: 'blue',
-                      }}
-                      value={3}
-                    >
-                      3
-                    </Radio.Button>
-                    <Radio.Button
-                      style={{
-                        border: '1px solid blue',
-                        padding: '0 25px 25px',
-                        color: 'blue',
-                      }}
-                      value={4}
-                    >
-                      4
-                    </Radio.Button>
-                    <Radio.Button
-                      style={{
-                        border: '1px solid blue',
-                        padding: '0 25px 25px',
-                        color: 'blue',
-                      }}
-                      value={5}
-                    >
-                      5
-                    </Radio.Button>
-                    <Radio.Button
-                      style={{
-                        border: '1px solid blue',
-                        padding: '0 25px 25px',
-                        color: 'blue',
-                      }}
-                      value={6}
-                    >
-                      6
-                    </Radio.Button>
-                    <Radio.Button
-                      style={{
-                        border: '1px solid blue',
-                        padding: '0 25px 25px',
-                        color: 'blue',
-                      }}
-                      value={7}
-                    >
-                      7
-                    </Radio.Button>
-                    <Radio.Button
-                      style={{
-                        border: '1px solid blue',
-                        padding: '0 25px 25px',
-                        color: 'blue',
-                      }}
-                      value={8}
-                    >
-                      8
-                    </Radio.Button>
-                    <Radio.Button
-                      style={{
-                        border: '1px solid blue',
-                        padding: '0 25px 25px',
-                        color: 'blue',
-                      }}
-                      value={9}
-                    >
-                      9
-                    </Radio.Button>
-                    <Radio.Button
-                      style={{
-                        border: '1px solid blue',
-                        padding: '0 25px 25px',
-                        color: 'blue',
-                      }}
-                      value={10}
-                    >
-                      10
-                    </Radio.Button>
-                  </Space>
-                </Radio.Group>
-                <Row>
-                  <Col span={10}>
-                    <Text style={{ fontSize: '12px', marginLeft: '-220px' }}>
-                      Very unlikely
-                    </Text>
-                  </Col>
-                  <Col span={10}>
-                    <Text style={{ fontSize: '12px', marginRight: '-530px' }}>
-                      Very likely
-                    </Text>
-                  </Col>
-                </Row>
+                      <Radio.Button
+                        style={{
+                          border: '1px solid #6b91f6',
+                          padding: '0 20px 40px',
+                          color: '#6b91f6',
+                        }}
+                        value={1}
+                      >
+                        1
+                      </Radio.Button>
+                      <Radio.Button
+                        style={{
+                          border: '1px solid #6b91f6',
+                          padding: '0 20px 40px',
+                          color: '#6b91f6',
+                        }}
+                        value={2}
+                      >
+                        2
+                      </Radio.Button>
+                      <Radio.Button
+                        style={{
+                          border: '1px solid #6b91f6',
+                          padding: '0 20px 40px',
+                          color: '#6b91f6',
+                        }}
+                        value={3}
+                      >
+                        3
+                      </Radio.Button>
+                      <Radio.Button
+                        style={{
+                          border: '1px solid #6b91f6',
+                          padding: '0 20px 40px',
+                          color: '#6b91f6',
+                        }}
+                        value={4}
+                      >
+                        4
+                      </Radio.Button>
+                      <Radio.Button
+                        style={{
+                          border: '1px solid #6b91f6',
+                          padding: '0 20px 40px',
+                          color: '#6b91f6',
+                        }}
+                        value={5}
+                      >
+                        5
+                      </Radio.Button>
+                      <Radio.Button
+                        style={{
+                          border: '1px solid #6b91f6',
+                          padding: '0 20px 40px',
+                          color: '#6b91f6',
+                        }}
+                        value={6}
+                      >
+                        6
+                      </Radio.Button>
+                      <Radio.Button
+                        style={{
+                          border: '1px solid #6b91f6',
+                          padding: '0 20px 40px',
+                          color: '#6b91f6',
+                        }}
+                        value={7}
+                      >
+                        7
+                      </Radio.Button>
+                      <Radio.Button
+                        style={{
+                          border: '1px solid #6b91f6',
+                          padding: '0 20px 40px',
+                          color: '#6b91f6',
+                        }}
+                        value={8}
+                      >
+                        8
+                      </Radio.Button>
+                      <Radio.Button
+                        style={{
+                          border: '1px solid #6b91f6',
+                          padding: '0 20px 40px',
+                          color: '#6b91f6',
+                        }}
+                        value={9}
+                      >
+                        9
+                      </Radio.Button>
+                      <Radio.Button
+                        style={{
+                          border: '1px solid #6b91f6',
+                          padding: '0 20px 40px',
+                          color: '#6b91f6',
+                        }}
+                        value={10}
+                      >
+                        10
+                      </Radio.Button>
+                    </Space>
+                  </Radio.Group>
+                  <Row>
+                    <Col span={10}>
+                      <Text
+                        style={{
+                          fontSize: '11px',
+                          fontWeight: 'bold',
+                          marginLeft: '-20px',
+                        }}
+                      >
+                        Very Unlikely
+                      </Text>
+                    </Col>
+                    <Col span={10}>
+                      <Text
+                        style={{
+                          fontSize: '11px',
+                          fontWeight: 'bold',
+                          marginRight: '-355px',
+                        }}
+                      >
+                        Very Likely
+                      </Text>
+                    </Col>
+                  </Row>
+                </div>
               </div>
             </Card>
           </div>
